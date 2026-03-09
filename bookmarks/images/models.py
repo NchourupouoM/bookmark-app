db64e4d7cc92d63c867f642b010d7230d02a0557
NchourupouoM <nchourupouom04@gmail.com>
2026-03-09 21:50:10 +0100

    feat: created the images application and added the image model

---
+++ bookmarks/images/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='images_created'
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=200)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    desciption = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
